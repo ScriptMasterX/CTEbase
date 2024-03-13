7b0083fd03bd6bfc2024464acd20f0806123acd9
TheMemeLover <109765300+TheMemeLover@users.noreply.github.com>
2024-03-12 18:06:31 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Login from './Login'
 
 export default function App() {
   return (
-    <main style={{overflowX: 'hidden', overflowY: 'hidden'}}>
-      <div className="navbar">
-        <h1>CTEbase</h1>
-      </div>
+    <main style={{overflow: 'hidden'}}>
+      
       <Login />
       
     </main>
